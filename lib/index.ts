bf5173563142e80df0b4cc84575691381e3ef0a3
Joseph Arriaza <joseph.arriaza@viaro.net>
2018-09-13 08:02:46 -0600

    Allowing params in pagination

--- lib/index.ts
+++ lib/index.ts
@@ -4,17 +4,26 @@ export class PaginationParser {
         let typeDefsWithPaging = typeDefs;
         paginationTypes.forEach( paginationType => {
             let toBeReplaced = '';
+            let params = ', ';
+            let isParentesisOpened = false;
             for (var i=typeDefs.indexOf(paginationType.functionName);i<typeDefs.length;i++) {
                 if (typeDefs[i] == '\n') {
                     break;
                 } else {
-                    toBeReplaced += typeDefs[i];
+                    if (typeDefs[i] == '(' ) {
+                        isParentesisOpened = true;
+                    } else if (typeDefs[i] == ')')  {
+                        isParentesisOpened = false;
+                    } else if (!isParentesisOpened) {
+                        toBeReplaced += typeDefs[i];
+                    } else if (isParentesisOpened) {
+                        params += typeDefs[i];
+                    }
                 }
             }
             typeDefsWithPaging = typeDefsWithPaging.replace(toBeReplaced, toBeReplaced.replace(' [' + paginationType.objectType + ']', ' ' + paginationType.objectType + 'Pager'));
             typeDefsWithPaging += '\ntype '+ paginationType.objectType +'Pager {\n  data: ['+ paginationType.objectType +']\n  pagination: Pagination\n}\n';
-            typeDefsWithPaging = typeDefsWithPaging.replace(paginationType.functionName, paginationType.functionName + '(pageIndex: Int, pageSize: Int)');
-
+            typeDefsWithPaging = typeDefsWithPaging.replace(paginationType.functionName, paginationType.functionName + '(pageIndex: Int, pageSize: Int ' + params + ')');
         });
 
         return typeDefsWithPaging;
